97709fee00dbef18d57827e7972afa60603b2bd4
Laure Duhorane <laure.duhorane@lewagon.org>
2020-08-08 12:36:30 -0400

    CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title)
+    params.require(:task).permit(:title, :details)
   end
 
   def set_task
